b319762b2b30845714ac4d272e501101c45afc2c
Robert van der Mark <robert.vandermark@gmail.com>
2016-09-26 11:16:00 -0400

    added templates and highlighting for summary section.  Moved seeding to rootScope

--- app.js
+++ app.js
@@ -10,35 +10,49 @@ angular.module('app')
 
 		.state('regions',{
 			url: '/regions',
-			templateUrl:'./templates/regions.html'
+			templateUrl:'./templates/regions.html',
+			controller: 'regionCtlr'
 		})
 
 		.state('salesPeople',{
 			url:'/salesPeople',
-			templateUrl:'./templates/salespeople.html'
+			templateUrl:'./templates/salespeople.html',
+			controller:'salesPersonCtlr'
 		})
 
 		$urlRouterProvider.otherwise('/')	
 	})
 
-app.controller('salesCtlr', function($scope){
+app.controller('onPageLoad',function($scope, $rootScope){
+	//On page load, seed the data.
+	$rootScope.regions = [
+		{id: 1, zipcode: '10530'},
+		{id: 2, zipcode: '12345'},
+		{id: 3, zipcode: '67890'},
+	];
+
+	$rootScope.salesPeople = [
+		{id: 1, name: 'mo'},
+		{id: 2, name: 'larry'},
+		{id: 3, name: 'curly'},
+	];
+})
+
+app.controller('summaryCtlr', function($scope, $rootScope){
+	//Prime the summary section
+	$scope.totalSalesPeople = $scope.salesPeople.length;
+	$scope.numberRegions = $scope.regions.length;
+
+	//update the summary section after creates, deletes.
 	$scope.$on('numberSalesPeople',function(obj, num){
 		$scope.totalSalesPeople = num;
 	});
 	$scope.$on('numberRegions', function(obj, num){
 		$scope.numberRegions = num;
-	});
-	
+	});	
 }); 
 
 app.controller('regionCtlr',function($scope, $rootScope){
-	$scope.regions = [
-		{id: 1, zipcode: '10530'},
-		{id: 2, zipcode: '12345'},
-		{id: 3, zipcode: '67890'},
-	];
-	$rootScope.$broadcast('numberRegions', $scope.regions.length)
-
 	$scope.destroy = function(region){
 		var idx = $scope.regions.indexOf(region);
 		$scope.regions.splice(idx,1);
@@ -52,21 +66,12 @@ app.controller('regionCtlr',function($scope, $rootScope){
 			return mem;
 		}, 0)
 		$scope.regions.push({id: ++max, zipcode: $scope.zipcode});
-		console.log($scope.regions)
 		$scope.zipcode = '';
 		$rootScope.$broadcast('numberRegions', $scope.regions.length);
 	}
 });
 
 app.controller('salesPersonCtlr', function($scope, $rootScope){
-	$scope.salesPeople = [
-		{id: 1, name: 'mo'},
-		{id: 2, name: 'larry'},
-		{id: 3, name: 'curly'},
-	];
-
-	$rootScope.$broadcast('numberSalesPeople', $scope.salesPeople.length)
-
 	$scope.destroy = function(salesPerson){
 		var idx = $scope.salesPeople.indexOf(salesPerson);
 		$scope.salesPeople.splice(idx,1);
